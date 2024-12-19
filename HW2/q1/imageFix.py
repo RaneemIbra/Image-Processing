027294b380f7721e3dbce343006ad4ec8c2d0968
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-12-19 09:24:18 +0200

    question 1 is done in HW 2

--- HW2/q1/imageFix.py
+++ HW2/q1/imageFix.py
@@ -6,31 +6,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def apply_fix(image, id):
-    # i chose for fixing the first image histogram equalization
-    if(id==1):
-        equalized_image = cv2.equalizeHist(image)
-        return equalized_image
+    # to fix the first image we performed a histogram equalization
+    if id==1:
+        return cv2.equalizeHist(image)
 
-    # i chose for fixing the second image  brightness and contrast stretching with alpha = 1.35 and beta = 25
-    if(id==2):
-        alpha = 1.35
-        beta = 25
-        stretched_image = np.clip(alpha * image + beta, 0, 255).astype(np.uint8)
-        return stretched_image
+    # to fix the second image we adjusted the brightness and contrast
+    elif id==2:
+        alpha, beta = 1.35, 25
+        return np.clip(alpha * image + beta, 0, 255).astype(np.uint8)
     
-    # i chose for fixing the third image  gamma correction
-    if(id==3):
+    # we chose to fix the last image with gamma correction
+    elif(id==3):
         gamma = 1.1
-        corrected_image = np.clip(image ** (1 / gamma), 0, 255).astype(np.uint8)
-        return corrected_image
+        inv_gamma = 1.0 / gamma
+        lookup_table = (np.arange(256) ** inv_gamma).clip(0, 255).astype(np.uint8)
+        return cv2.LUT(image, lookup_table)
+
+    else:
+        raise ValueError(f"Invalid fix_type {id}. Supported values are 1, 2, or 3.")
 
 
 for i in range(1, 4):
     if i == 1:
-        path = f'q1\\{i}.png'  # First image is assumed to be a PNG file
+        path = f'q1\\{i}.png'
     else:
-        path = f'q1\\{i}.jpg'  # The other images are assumed to be JPG files
-    
+        path = f'q1\\{i}.jpg'
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     fixed_image = apply_fix(image, i)
     plt.imsave(f'{i}_fixed.jpg', fixed_image, cmap='gray', vmin=0, vmax=255)
