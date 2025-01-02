64a251184dfb8b2e956bb8aeb314a161ec391981
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2025-01-02 19:08:59 +0200

    finished the code, all that is left is the report

--- HW4/q2/zebra.py
+++ HW4/q2/zebra.py
@@ -13,10 +13,13 @@ image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # Your code goes here
-# section a, where we get the fourier transform of the image and then show it
+# first get the width and height of the image
+Height, Width = image.shape
+# Section A:
+# compute the fourier transform of the image and the shifted fourier transform and the fourier spectrum
 fourier_transform = fft2(image)
-shifted_fourier_transform = fftshift(fourier_transform)
-fourier_spectrum = np.log(1 + np.abs(shifted_fourier_transform))
+fourier_transform_shifted = fftshift(fourier_transform)
+fourier_spectrum = np.log(1 + np.abs(fourier_transform_shifted))
 
 plt.figure(figsize=(10,10))
 plt.subplot(321)
@@ -27,39 +30,43 @@ plt.subplot(322)
 plt.title('Fourier Spectrum')
 plt.imshow(fourier_spectrum, cmap='gray')
 
-# section b, where we get the image height and width and then padd it
-# then we apply the same steps as before
-Height, Width = image.shape
-padded_image = np.pad(image, ((0, Height), (0, Width)), mode='constant')
-padded_fourier_transform = fft2(padded_image)
-shifted_padded_fourier_transform = fftshift(padded_fourier_transform)
-padded_fourier_spectrum = np.log(1 + np.abs(shifted_padded_fourier_transform))
+# Section B:
+# zero pad the fourier transform and compute the inverse fourier transform to restore the image
+padded_fourier_transform = np.zeros((2*Height, 2*Width), dtype=np.complex64)
+centerH, centerW = Height // 2, Width // 2
+padded_fourier_transform[Height-centerH : Height+centerH, Width-centerW : Width+centerW] = fourier_transform_shifted
+
+inverse_shifted = ifftshift(padded_fourier_transform)
+restored_spatial = ifft2(inverse_shifted)
+scaled_image = np.abs(restored_spatial)
+scaled_image *= 4
+
+# clip the image to be in the range of 0-255 and convert it to 8-bit
+scaled_image_8bit = np.clip(scaled_image, 0, 255).astype(np.uint8)
+log_spectrum_padded = np.log(1 + np.abs(padded_fourier_transform))
 
 plt.subplot(323)
 plt.title('Fourier Spectrum Zero Padding')
-plt.imshow(padded_fourier_spectrum, cmap='gray')
+plt.imshow(log_spectrum_padded, cmap='gray')
 
 plt.subplot(324)
 plt.title('Two Times Larger Grayscale Image')
-plt.imshow(padded_image, cmap='gray')
+plt.imshow(scaled_image_8bit, cmap='gray')
 
-# section c, where we use the fourier transform scale formula to generate an image with size (2*H, 2*W) containing 4 copies of the zebra
-scaled_fourier_transform = np.zeros((2*Height, 2*Width), dtype=complex)
-scaled_fourier_transform[:Height//2, :Width//2] = shifted_fourier_transform[Height//2:, Width//2:]
-scaled_fourier_transform[:Height//2, -Width//2:] = shifted_fourier_transform[Height//2:, :Width//2]
-scaled_fourier_transform[-Height//2:, :Width//2] = shifted_fourier_transform[:Height//2, Width//2:]
-scaled_fourier_transform[-Height//2:, -Width//2:] = shifted_fourier_transform[:Height//2, :Width//2]
-
-# then we restore the image using the inverse fourier transform
-scaled_image = np.abs(ifft2(ifftshift(scaled_fourier_transform)))
-scaled_fourier_spectrum = np.log(1 + np.abs(scaled_fourier_transform))
+# Section C:
+# scale the image by 0.5 and create four copies of it and compute the fourier transform and the fourier spectrum
+half_image = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+built_image = np.tile(half_image, (2, 2))
+fourier_transform_4copies = fft2(built_image)
+fourier_transform_shifted_4copies = fftshift(fourier_transform_4copies)
+fourier_spectrum_4copies = np.log(1 + np.abs(fourier_transform_shifted_4copies))
 
 plt.subplot(325)
 plt.title('Fourier Spectrum Four Copies')
-plt.imshow(scaled_fourier_spectrum, cmap='gray')
+plt.imshow(fourier_spectrum_4copies, cmap='gray')
 
 plt.subplot(326)
 plt.title('Four Copies Grayscale Image')
-plt.imshow(scaled_image, cmap='gray')
+plt.imshow(built_image, cmap='gray')
 plt.show()
 # plt.savefig('zebra_scaled.png')
